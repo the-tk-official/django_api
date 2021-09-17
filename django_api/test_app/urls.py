295f49c990dceb19cbbd9d6d607c6a4add292193
the.tk.official <the.tk.official@gmail.com>
2021-09-17 19:30:11 +0500

    Working with request methods on class

--- django_api/test_app/urls.py
+++ django_api/test_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from test_app.views import simple
+from test_app.views import Simple
 
 urlpatterns = [
-    path('simple/', simple)
+    path('simple/', Simple.as_view())
 ]
--- django_api/test_app/views.py
+++ django_api/test_app/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse, response
+from django.forms.models import model_to_dict
+from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
+
+from test_app.models import TestModel
 
 # Create your views here.
 
-@csrf_exempt
-def simple(request):
-    # perform operations
-    method = request.method.lower()
-    if method == 'get':
-        return JsonResponse({'data': [1, 2, 3, 4, 5]})
-    elif method == "post":
-        return JsonResponse({'data': 'Added data successfully!'})
-    elif method == 'put':
-        return JsonResponse({'data': 'Updated data successfully!'})
-    return JsonResponse({'error': 'Method not allowed'})
+class Simple(APIView):
+
+    def post(self, request):
+        new_test_content = TestModel.objects.create(
+            name=request.data['name'],
+            description=request.data['description'],
+            phone_number=request.data['phone_number'],
+            is_alive=request.data['is_alive'],
+            amount=request.data['amount']
+        )
+        return JsonResponse({'data': model_to_dict(new_test_content)})
+
+    def get(self, request):
+        content = TestModel.objects.all().values()
+        return JsonResponse({'data': list(content)})
