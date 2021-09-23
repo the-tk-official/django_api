30ab26685b29dd7d8c318fb518a4aec3c8d3b405
the.tk.official <the.tk.official@gmail.com>
2021-09-23 21:30:29 +0500

    Creating custom user model for user application

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
     # My apps
     'test_app.apps.TestAppConfig',
-    'user.apps.UserAppConfig',
+    'user.apps.UserConfig',
 
     # Rest_framework
     'rest_framework',
@@ -166,3 +166,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
